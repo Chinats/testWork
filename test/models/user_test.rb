dc8f1e492a613d3df1182c6b828cbf9a9c3db7c7
kanayama <knym.777@gmail.com>
2018-03-31 19:37:39 +0900

    テスト修正:saveではなくvalidでエラーを判定

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "名前が空ならエラー" do
-    user = User.new
-    assert_not user.save
+  test "名前が空ならvalidateでエラーになる" do
+    user = users(:two)
+    assert_not user.valid?
+    assert_equal user.errors.details, {:name=>[{:error=>:blank}]}
   end
 end
