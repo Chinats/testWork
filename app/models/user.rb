797da1b6e052b4b647360fa2470a0f406f3e0566
kanayama <knym.777@gmail.com>
2018-04-12 17:52:46 +0900

    テストとバリデーとの追加: メールアドレスのフォーマットが異なっていた場合にエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+  validates :email, format: { with: VALID_EMAIL_REGEX , message: 'メールアドレスが適切でありません。'}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.valid?
     assert_equal user.errors.details, {:name=>[{:error=>:blank}]}
   end
+  test "メールアドレスに@が含まれていなければvalidateでエラーになる" do
+    user = users(:two)
+    assert_not user.valid?
+  end
 end
